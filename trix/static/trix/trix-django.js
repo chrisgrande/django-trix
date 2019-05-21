66c3f83589432f53700256d44d9969365d50e687
Christopher Grande <chrisgrande@mac.com>
2019-05-20 20:18:08 -0400

    Replace var with let

--- trix/static/trix/trix-django.js
+++ trix/static/trix/trix-django.js
@@ -7,11 +7,11 @@ Trix.config.attachments.preview.caption = {
 // Function to get local cookies
 // Used to return the csrf token with: getCookie('csrftoken');
 function getCookie(name) {
-    var cookieValue = null;
+    let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
         const cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim();
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
             // Does this cookie string begin with the name we want?
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
@@ -24,26 +24,26 @@ function getCookie(name) {
 
 function uploadAttachment(attachment) {
     // Create form data to submit
-    var file = attachment.file;
-    var form = new FormData;
+    let file = attachment.file;
+    let form = new FormData;
     form.append("Content-Type", file.type);
     form.append("trix-attachment", file);
 
     // Create XHR request
-    var request = new XMLHttpRequest();
+    let request = new XMLHttpRequest();
     request.open('POST', '/trix/attachment/', true);
     request.setRequestHeader("X-CSRF-Token", getCookie('csrftoken'));
 
     // Report file uploads back to Trix
     request.upload.onprogress = function(event) {
-        var progress = event.loaded / event.total * 100;
+        let progress = event.loaded / event.total * 100;
         attachment.setUploadProgress(progress);
     };
 
     // Tell Trix what url and href to use on successful upload
     request.onload = function() {
         if (request.status === 200) {
-            var data = JSON.parse(request.responseText);
+            let data = JSON.parse(request.responseText);
             return attachment.setAttributes({
                 url: data.attachment_url,
             })
@@ -54,7 +54,7 @@ function uploadAttachment(attachment) {
 }
 
 document.addEventListener('trix-attachment-add', function(event) {
-    var attachment = event.attachment;
+    let attachment = event.attachment;
     if (attachment.file) {
         return uploadAttachment(attachment);
     }
@@ -62,9 +62,9 @@ document.addEventListener('trix-attachment-add', function(event) {
 
 // Fix for FormSet __prefix__
 addEventListener("trix-initialize", function(event) {
-  var trixElement = event.target;
+  let trixElement = event.target;
   if (trixElement.getAttribute("input") === "id_child_set-__prefix__-content") {
-    var inputElement = trixElement.closest(".field-content").querySelector("textarea");
+    let inputElement = trixElement.closest(".field-content").querySelector("textarea");
     trixElement.setAttribute("input", inputElement.id)
   }
 });
