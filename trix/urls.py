6c58072e015dbc354993fc3aa790ea49f8d80b92
Christopher Grande <chrisgrande@users.noreply.github.com>
2025-02-19 19:27:59 -0500

    Upgrade for Django 4+
    Works with 3 and 4.

--- trix/urls.py
+++ trix/urls.py
@@ -1,6 +1,12 @@
-from django.conf.urls import include, url
-from .views import UploadTrixAttachment
+try:
+    # Django 3.x
+    from django.conf.urls import url
+except ImportError:
+    # Django 4.x+
+    from django.urls import re_path as url
 
+from django.urls import include
+from .views import UploadTrixAttachment
 
 urlpatterns = [
     url(r'^attachment/$', UploadTrixAttachment.as_view()),
