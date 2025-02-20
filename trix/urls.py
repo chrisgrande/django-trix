49569984c39c3d1eccd795e5f48039da51604e9a
Christopher Grande <chrisgrande@mac.com>
2025-02-19 20:46:02 -0500

    Revert "Upgrade for Django 4+"
    This reverts commit 6c58072e015dbc354993fc3aa790ea49f8d80b92.

--- trix/urls.py
+++ trix/urls.py
@@ -1,12 +1,6 @@
-try:
-    # Django 3.x
-    from django.conf.urls import url
-except ImportError:
-    # Django 4.x+
-    from django.urls import re_path as url
-
-from django.urls import include
+from django.conf.urls import include, url
 from .views import UploadTrixAttachment
+
 
 urlpatterns = [
     url(r'^attachment/$', UploadTrixAttachment.as_view()),
